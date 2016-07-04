741ec356be638184cda5c879921edc6cbc184a8a
ratov <ratov-kirill84@yandex.ru>
2016-07-04 20:09:31 +0300

    Модифецировал класс abstractselect_class.php

--- core/abstractselect_class.php
+++ core/abstractselect_class.php
@@ -37,4 +37,77 @@ class AbstractSelect
         $from .= " FROM `$table_name`";
         return $this;
     }
+
+    public function where($where, $values = array(), $and = true)
+    {
+        if($where) {
+            $where = $this->db->getQuery($where, $values);
+            $this->addWhere($where, $and);
+        }
+    }
+
+    public function whereIn($field, $values, $and = true)
+    {
+        $where = "`$field` IN (";
+        foreach($values as $value) {
+            $where .= $this->db->getSQ() . ",";
+        }
+        $where = substr($where, 0, -1);
+        $where .= ")";
+        return $this->where($where, $values, $and);
+    }
+
+    public function order($field, $ask = true)
+    {
+        if(is_array($field)) {
+            $this->order = "ORDER BY ";
+            if(!is_array($ask)) {
+                $temp = array();
+                for($i = 0; $i < count($field); $i++) $temp[] = $ask;
+                $ask = $temp;
+            }
+            for($i = 0; $i < count($field); $i++) {
+                $this->order .= "`" . $field[$i] . "`";
+                if(!$ask[$i]) $this->order .= " DESC,";
+                else $this->order .= ",";
+            }
+            $this->order = substr($this->order, 0, -1);
+        } else {
+            $this->order = "ORDER BY `$field`";
+            if(!$ask) $this->order .= " DESC";
+        }
+        return $this;
+    }
+
+    public function limit($count, $offset = 0)
+    {
+        $count = (int)$count;
+        $offset = (int)$offset;
+        if($count < 0 || $offset < 0) return false;
+        $this->limit = "LIMIT $offset, $count";
+        return $this;
+    }
+
+    public function rand()
+    {
+        $this->order = "ORDER BY RAND(";
+    }
+
+    public function __toString()
+    {
+      if($this->from)
+          $ret = "SELECT " . $this->from . " " . $this->where . " " . $this->order . " " . $this->limit;
+        else $ret = "";
+        return $ret;
+    }
+
+    private function addWhere($where, $and)
+    {
+        if($this->where) {
+            if($and) $this->where .= " AND ";
+            else $this->where .= " OR ";
+            $this->where .= $where;
+        }
+        else $this->where = "WHERE $where";
+    }
 }
